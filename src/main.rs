bc3aee1d04ff22a71c4fecbee52bbcfdd6035cd8
Ryan Barry <ryan@nuclearice.com>
2024-02-23 15:48:46 -0800

    add a question section to the response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::net::UdpSocket;
 
+use bytes::{BufMut, BytesMut};
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -11,15 +13,23 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [
+                let header = [
                     4u8, 210u8, // packet id
                     0b10000000, // QR, OPCODE, AA, TC, RD
                     0b00000000, // RA, Z, RCODE
-                    0u8, 0u8,   // QDCOUNT
+                    0u8, 1u8,   // QDCOUNT
                     0u8, 0u8,   // ANCOUNT
                     0u8, 0u8,   // NSCOUNT
                     0u8, 0u8,   // ARCOUNT
                 ];
+                let mut response = BytesMut::from(&header[..]);
+
+                response.put(&b"\x0ccodecrafters\x02io"[..]);
+                response.put_u8(0u8); // null byte to end the label sequence that is QNAME
+
+                response.put_u16(1u16); // QTYPE for A record
+                response.put_u16(1u16); // QCLASS for IN
+
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
