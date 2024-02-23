4ffce6b0a41d92f0512f406eff23a67bb9bf6fbd
Ryan Barry <ryan@nuclearice.com>
2024-02-22 19:56:45 -0800

    hardcode simple DNS message to send

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,14 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response = [
+                    34u8, 12u8, // packet id
+                    0b10000000, // QR, OPCODE, AA, TC, RD, RA, Z, RCODE
+                    0u8, 0u8,   // QDCOUNT
+                    0u8, 0u8,   // ANCOUNT
+                    0u8, 0u8,   // NSCOUNT
+                    0u8, 0u8,   // ARCOUNT
+                ];
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
